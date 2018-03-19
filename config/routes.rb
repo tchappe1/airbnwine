ff96e500ec2a0c928fce3fed0b88302d55547525
Thierry Chappe <tchappe@me.com>
2018-03-19 15:03:31 +0100

    Vinc controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'vins/index'
+
+  get 'vins/show'
+
+  get 'vins/edit'
+
+  get 'vins/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
