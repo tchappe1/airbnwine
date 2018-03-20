1915cf64590e2b0bb5669819b5d6f7481e4c2028
yawuda <yawudatrigoteixeira@gmail.com>
2018-03-20 12:17:20 +0100

    update delete function added to the pages (#5)
    ok

--- app/controllers/vins_controller.rb
+++ app/controllers/vins_controller.rb
@@ -12,25 +12,45 @@ before_action :set_vin, except: [:index, :new]
   end
 
   def new
+    @vin = Vin.new(vin_params)
   end
 
   def create
+    @vin = vin.new(vin_params)
+      if @vin.save
+        redirect_to vins_path
+      else
+        render :new
+      end
   end
 
   def update
+    respond_to do |format|
+      if @vin.update(vin_params)
+        format.html { redirect_to @vin, notice: 'vin was successfully updated.' }
+        format.json { render :show, status: :ok, location: @vin }
+      else
+        format.html { render :edit }
+        format.json { render json: @vin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @vin.destroy
+    respond_to do |format|
+      format.html { redirect_to vins_url, notice: 'Vin was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
-
-end
 
 private
 
-def set_vin
-  @vin = Vin.find(params[:id])
-end
+  def set_vin
+    @vin = Vin.find(params[:id])
+  end
 
-def vin_params
-  params.require(:restaurant).permit(:region, :appelation, :vignoble, :producteur, :alcool , :parker, :jrobinson, :prix_btl, :annee, :nb_btl_caisse)
+  def vin_params
+    params.require(:vin).permit(:region, :appelation, :vignoble, :producteur, :alcool , :parker, :jrobinson, :prix_btl, :annee, :nb_btl_caisse, :description)
+  end
 end
