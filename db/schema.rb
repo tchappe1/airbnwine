7b0f78d209cc354b9b72566f14dc123c29d681bf
yawuda <yawudatrigoteixeira@gmail.com>
2018-03-23 12:35:41 +0100

    fix schema reviews fk (#22)
    * model created
    
    * creating migration and correction db schema
    
    * simple view works

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320152639) do
+ActiveRecord::Schema.define(version: 20180323112740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "vin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["vin_id"], name: "index_reviews_on_vin_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,4 +57,5 @@ ActiveRecord::Schema.define(version: 20180320152639) do
     t.string "photo"
   end
 
+  add_foreign_key "reviews", "vins"
 end
