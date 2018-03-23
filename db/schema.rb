8c32cc6fcf556ead53338b0ea2c47fa6b2e03d9d
yawuda luzaki <yawudatrigoteixeira@gmail.com>
2018-03-23 11:13:28 +0100

    model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320152639) do
+ActiveRecord::Schema.define(version: 20180323101254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "vins_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["vins_id"], name: "index_reviews_on_vins_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,4 +57,5 @@ ActiveRecord::Schema.define(version: 20180320152639) do
     t.string "photo"
   end
 
+  add_foreign_key "reviews", "vins", column: "vins_id"
 end
